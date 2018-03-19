1a12907b674a8179d97c1237af0dd324ae2d9710
Ruslan Akbashev <ruslashev@gmail.com>
2018-03-20 02:16:19 +0300

    Fuck

--- client/main.cc
+++ client/main.cc
@@ -5,14 +5,59 @@
 #include "../3rdparty/imgui/imgui.h"
 
 static bool g_done = false;
+GLuint vao, vbo, vertexShader, fragmentShader, shaderProgram;
 
 static void load() {
   glClearColor(5.f / 255.f, 5.f / 255.f, 5.f / 255.f, 1.f);
+
+  const char *vert_shader_src =
+    "#version 120 \n"
+    "attribute vec2 position; \n"
+    "void main() { \n"
+    "  gl_Position = vec4(position, 0.0, 1.0); \n"
+    "} \n";
+  const char *frag_shader_src =
+    "#version 120 \n"
+    "void main() { \n"
+    "  gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0); \n"
+    "} \n";
+
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
+
+  glGenBuffers(1, &vbo);
+
+  GLfloat vertices[] = {
+     0.0f,  0.5f,
+     0.5f, -0.5f,
+    -0.5f, -0.5f
+  };
+
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vert_shader_src, NULL);
+  glCompileShader(vertexShader);
+
+  fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &frag_shader_src, NULL);
+  glCompileShader(fragmentShader);
+
+  shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+  glUseProgram(shaderProgram);
+
+  GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+  glEnableVertexAttribArray(posAttrib);
+  glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
 }
 
 static void draw() {
   glClear(GL_COLOR_BUFFER_BIT);
-  ImGui::ShowTestWindow();
+  glDrawArrays(GL_TRIANGLES, 0, 3);
 }
 
 static void update(double dt, double t) {
@@ -22,6 +67,13 @@ static void key_event(unsigned long long key, bool down) {
 }
 
 static void destroy() {
+  glDeleteProgram(shaderProgram);
+  glDeleteShader(fragmentShader);
+  glDeleteShader(vertexShader);
+
+  glDeleteBuffers(1, &vbo);
+
+  glDeleteVertexArrays(1, &vao);
 }
 
 int main() {
