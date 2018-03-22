f7343a2fc3de3e20b07a4d443e9dfaf7a6e799a9
Ruslan Akbashev <ruslashev@gmail.com>
2018-03-22 13:45:33 +0300

    Reading shaders and error handling

--- client/main.cc
+++ client/main.cc
@@ -1,25 +1,41 @@
 #include <cstdio>
+#include <vector>
 #include <GL/glew.h>
 #include "gfx.hh"
+#include "../common/utils.hh"
 
 static bool g_done = false;
-GLuint vao, vbo, vertexShader, fragmentShader, shaderProgram;
+GLuint vao, vbo, vert_shader, frag_shader, shader_program;
+
+GLuint compile_shader(GLuint type, const std::string &source) {
+  GLuint id = glCreateShader(type);
+
+  const char *source_cstr = source.c_str();
+  glShaderSource(id, 1, &source_cstr, nullptr);
+  glCompileShader(id);
+
+  GLint compile_status;
+  glGetShaderiv(id, GL_COMPILE_STATUS, &compile_status);
+
+  if (compile_status)
+    return id;
+
+  GLint log_len;
+  glGetShaderiv(id, GL_INFO_LOG_LENGTH, &log_len);
+
+  std::vector<char> info_log(log_len);
+  glGetShaderInfoLog(id, log_len, nullptr, &info_log[0]);
+  std::string info_log_str(info_log.begin(), info_log.end());
+
+  glDeleteShader(id);
+
+  die("Failed to compile %s shader:\n%s"
+      , (type == GL_VERTEX_SHADER) ? "vertex" : "fragment", info_log_str.c_str());
+}
 
 static void load() {
   glGenVertexArrays(1, &vao);
   glBindVertexArray(vao);
-
-  const char *vert_shader_src =
-    "#version 120 \n"
-    "attribute vec2 position; \n"
-    "void main() { \n"
-    "  gl_Position = vec4(position, 0.0, 1.0); \n"
-    "} \n";
-  const char *frag_shader_src =
-    "#version 120 \n"
-    "void main() { \n"
-    "  gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0); \n"
-    "} \n";
 
   glGenBuffers(1, &vbo);
 
@@ -32,23 +48,18 @@ static void load() {
   glBindBuffer(GL_ARRAY_BUFFER, vbo);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-  vertexShader = glCreateShader(GL_VERTEX_SHADER);
-  glShaderSource(vertexShader, 1, &vert_shader_src, NULL);
-  glCompileShader(vertexShader);
+  vert_shader = compile_shader(GL_VERTEX_SHADER, read_file("vsh.glsl"));
+  frag_shader = compile_shader(GL_FRAGMENT_SHADER, read_file("fsh.glsl"));
 
-  fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-  glShaderSource(fragmentShader, 1, &frag_shader_src, NULL);
-  glCompileShader(fragmentShader);
+  shader_program = glCreateProgram();
+  glAttachShader(shader_program, vert_shader);
+  glAttachShader(shader_program, frag_shader);
+  glLinkProgram(shader_program);
+  glUseProgram(shader_program);
 
-  shaderProgram = glCreateProgram();
-  glAttachShader(shaderProgram, vertexShader);
-  glAttachShader(shaderProgram, fragmentShader);
-  glLinkProgram(shaderProgram);
-  glUseProgram(shaderProgram);
-
-  GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
-  glEnableVertexAttribArray(posAttrib);
-  glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
+  GLint pos_attrib = glGetAttribLocation(shader_program, "position");
+  glEnableVertexAttribArray(pos_attrib);
+  glVertexAttribPointer(pos_attrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
 }
 
 static void draw() {
@@ -63,9 +74,9 @@ static void key_event(unsigned long long key, bool down) {
 }
 
 static void destroy() {
-  glDeleteProgram(shaderProgram);
-  glDeleteShader(fragmentShader);
-  glDeleteShader(vertexShader);
+  glDeleteProgram(shader_program);
+  glDeleteShader(frag_shader);
+  glDeleteShader(vert_shader);
 
   glDeleteBuffers(1, &vbo);
 
--- common/utils.hh
+++ common/utils.hh
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <string>
+#include <fstream>
+
 #ifdef __PRETTY_FUNCTION__
 #define info() \
   printf("%s:%d in %s", __FILE__, __LINE__, __PRETTY_FUNCTION__);
@@ -19,4 +22,13 @@
   } while (0)
 
 #define die(...) do { printf(__VA_ARGS__); puts(""); exit(1); } while (0)
+
+inline std::string read_file(const std::string &filename) {
+  std::ifstream ifs(filename, std::ios::binary);
+  if (!ifs)
+    die("Failed to open file \"%s\"", filename.c_str());
+  std::string buffer { std::istreambuf_iterator<char>(ifs)
+    , std::istreambuf_iterator<char>() };
+  return buffer;
+}
 
