4777aefc5c99b18edbb4c43484d5f4e75f5aa631
Ruslan Akbashev <ruslashev@gmail.com>
2018-03-24 15:50:34 +0300

    Fsh maintenance

--- fsh.glsl
+++ fsh.glsl
@@ -13,17 +13,28 @@ float sphere(vec3 p, float s) {
   return length(p) - s;
 }
 
-float scene(vec3 p) {
-  return sphere(p, 1.0);
+float ubox(vec3 p, vec3 b) {
+  return length(max(abs(p) - b, 0.0));
 }
 
-vec3 ray_direction(float vfov, vec2 screen, vec2 frag_coord) {
-  vec2 xy = frag_coord - screen / 2.0;
-  float z = screen.y / tan(radians(vfov) / 2.0);
+float rect(vec3 p, vec3 b) {
+  vec3 d = abs(p) - b;
+  float inside_distance = min(max(d.x, max(d.y, d.z)), 0.0);
+  float outside_distance = length(max(d, 0.0));
+  return inside_distance + outside_distance;
+}
+
+float scene(vec3 p) {
+  return rect(p, vec3(1.0, 1.0, 1.0));
+}
+
+vec3 ray_direction(float vfov, vec2 screen_size, vec2 frag_coord) {
+  vec2 xy = frag_coord - screen_size / 2.0;
+  float z = screen_size.y / tan(radians(vfov) / 2.0);
   return normalize(vec3(xy, -z));
 }
 
-float shortestDistanceToSurface(vec3 eye, vec3 dir, float start, float end) {
+float distance_to_scene(vec3 eye, vec3 dir, float start, float end) {
   float depth = start;
   for (int i = 0; i < MAX_STEPS; i++) {
     float dist = scene(eye + depth * dir);
@@ -38,7 +49,7 @@ float shortestDistanceToSurface(vec3 eye, vec3 dir, float start, float end) {
   return end;
 }
 
-vec3 estimateNormal(vec3 p) {
+vec3 estimate_normal(vec3 p) {
   return normalize(vec3(
     scene(vec3(p.x + EPS, p.y, p.z)) - scene(vec3(p.x - EPS, p.y, p.z)),
     scene(vec3(p.x, p.y + EPS, p.z)) - scene(vec3(p.x, p.y - EPS, p.z)),
@@ -46,13 +57,12 @@ vec3 estimateNormal(vec3 p) {
   ));
 }
 
-vec3 phongContribForLight(vec3 k_d, vec3 k_s, float alpha, vec3 p, vec3 eye,
-    vec3 lightPos, vec3 lightIntensity) {
-  vec3 N = estimateNormal(p);
-  vec3 L = normalize(lightPos - p);
+vec3 phong_contrib_for_light(vec3 Kd, vec3 Ks, float alpha, vec3 p, vec3 eye,
+    vec3 pos, vec3 intensity) {
+  vec3 N = estimate_normal(p);
+  vec3 L = normalize(pos - p);
   vec3 V = normalize(eye - p);
   vec3 R = normalize(reflect(-L, N));
-
   float dotLN = dot(L, N);
   float dotRV = dot(R, V);
 
@@ -64,43 +74,37 @@ vec3 phongContribForLight(vec3 k_d, vec3 k_s, float alpha, vec3 p, vec3 eye,
   if (dotRV < 0.0) {
     // Light reflection in opposite direction as viewer, apply only diffuse
     // component
-    return lightIntensity * (k_d * dotLN);
+    return intensity * (Kd * dotLN);
   }
-  return lightIntensity * (k_d * dotLN + k_s * pow(dotRV, alpha));
+
+  return intensity * (Kd * dotLN + Ks * pow(dotRV, alpha));
 }
 
-vec3 phongIllumination(vec3 k_a, vec3 k_d, vec3 k_s, float alpha, vec3 p, vec3 eye) {
-  const vec3 ambientLight = 0.5 * vec3(1.0, 1.0, 1.0);
-  vec3 color = ambientLight * k_a;
+vec3 phong_illuminate(vec3 Ka, vec3 Kd, vec3 Ks, float alpha, vec3 p, vec3 eye) {
+  const vec3 ambient_light = 0.5 * vec3(1.0, 1.0, 1.0);
+  vec3 color = ambient_light * Ka;
 
-  vec3 light1Pos = vec3(4.0 * sin(time),
-      2.0,
-      4.0 * cos(time));
-  vec3 light1Intensity = vec3(0.4, 0.4, 0.4);
+  vec3 light1_pos = vec3(4.0 * sin(time), 2.0, 4.0 * cos(time));
+  vec3 light1_intensity = vec3(0.4, 0.4, 0.4);
+  color += phong_contrib_for_light(Kd, Ks, alpha, p, eye, light1_pos,
+      light1_intensity);
 
-  color += phongContribForLight(k_d, k_s, alpha, p, eye,
-      light1Pos,
-      light1Intensity);
+  vec3 light2_pos = vec3(2.0 * sin(0.37 * time), 2.0 * cos(0.37 * time), 2.0);
+  vec3 light2_intensity = vec3(0.4, 0.4, 0.4);
+  color += phong_contrib_for_light(Kd, Ks, alpha, p, eye, light2_pos,
+      light2_intensity);
 
-  vec3 light2Pos = vec3(2.0 * sin(0.37 * time),
-      2.0 * cos(0.37 * time),
-      2.0);
-  vec3 light2Intensity = vec3(0.4, 0.4, 0.4);
-
-  color += phongContribForLight(k_d, k_s, alpha, p, eye,
-      light2Pos,
-      light2Intensity);
   return color;
 }
 
 float sinn(float x) {
-  return ((sin(x) + 1.0) / 2.0);
+  return (sin(x) + 1.0) / 2.0;
 }
 
 void main() {
   vec3 dir = ray_direction(45.0, resolution, gl_FragCoord.xy);
   vec3 eye = vec3(0.0, 0.0, 5.0);
-  float dist = shortestDistanceToSurface(eye, dir, MIN_DIST, MAX_DIST);
+  float dist = distance_to_scene(eye, dir, MIN_DIST, MAX_DIST);
 
   if (dist > MAX_DIST - EPS) {
     // no hit
@@ -110,12 +114,11 @@ void main() {
 
   vec3 p = eye + dist * dir;
 
-  vec3 K_a = vec3(0.2, 0.2, 0.2);
-  vec3 K_d = vec3(0.7, 0.2, 0.2);
-  vec3 K_s = vec3(1.0, 1.0, 1.0);
+  vec3 Ka = vec3(0.2, 0.2, 0.2);
+  vec3 Kd = vec3(0.7, 0.2, 0.2);
+  vec3 Ks = vec3(1.0, 1.0, 1.0);
   float shininess = 10.0;
-
-  vec3 color = phongIllumination(K_a, K_d, K_s, shininess, p, eye);
+  vec3 color = phong_illuminate(Ka, Kd, Ks, shininess, p, eye);
 
   gl_FragColor = vec4(color, 1.0);
 }
